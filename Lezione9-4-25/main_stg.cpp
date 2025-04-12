eba29ed55793bb59fe57aeda593f227e611ae339
FiorentinoLuca <lukfiorentino@gmail.com>
2025-04-12 02:39:36 +0200

    dfd

---
+++ Lezione9-4-25/main_stg.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "test.hpp"
+#include "test.hpp"
+
+using namespace std;
+
+int y = 19;
+
+int main() {
+
+    // const int y;
+    // const int x;
+
+
+
+
+
+
+    
+    cout << "call to function: #";
+
+    return 0;
+
+}
